ea8ece536b193b1e9e422a5427bd1bdc34c2917c
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-05-11 13:47:45 +0200

    ajout form connexion + /login

--- ProjectCinemaSecurityFront/src/app/app-routing.module.ts
+++ ProjectCinemaSecurityFront/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilmComponent } from './components/film/film.component';
 import { ListFilmComponent } from './components/list-film/list-film.component';
+import { LoginComponent } from './components/login/login.component';
 import { TicketComponent } from './components/ticket/ticket.component';
 
 const routes: Routes = [
     {path:'film/:id', component: FilmComponent},
     {path:'film/:id/ticket', component: TicketComponent},
+    {path:'login', component: LoginComponent},
     {path:'**', component: ListFilmComponent}
 ];
 
