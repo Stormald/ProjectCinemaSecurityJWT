71059fe164a2ca6788d148a26e36e13b28580478
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-05-12 12:24:54 +0200

    Ajout logout

--- ProjectCinemaSecurityFront/src/app/navbar/navbar.component.ts
+++ ProjectCinemaSecurityFront/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
+  
+  jwtHelper = new JwtHelperService();
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isUserAuthenticated = (): boolean => {
+    const token = localStorage.getItem("jwt");
+
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  logout(){
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("userId");
   }
 
 }
